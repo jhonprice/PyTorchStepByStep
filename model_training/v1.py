8d5d12ff5c6ad0889e2d2c1ed695e6dcc3b12aaf
jhonprice <a718329809@live.com>
2026-05-05 21:19:04 +0800

    学习

--- model_training/v1.py
+++ model_training/v1.py
@@ -1,11 +1,12 @@
 
-# Defines number of epochs
+# 定义周期数
 n_epochs = 1000
 
+# 记录每次循环的损失
 losses = []
 
 # For each epoch...
 for epoch in range(n_epochs):
-    # Performs one train step and returns the corresponding loss
+    # 随机梯度下降：全部训练数据，训练N遍
     loss = train_step_fn(x_train_tensor, y_train_tensor)
     losses.append(loss)
